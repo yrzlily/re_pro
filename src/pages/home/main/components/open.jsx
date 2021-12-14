ad2bf1c1a5b711a9ea5d8923b917b0d60815713a
yangruizhou <1021454559@qq.com>
2021-12-14 14:03:30 +0800

    修改表单

--- src/pages/home/main/components/open.jsx
+++ src/pages/home/main/components/open.jsx
@@ -1,5 +1,18 @@
-import {useState, Component} from 'react'
+import {Component} from 'react'
+import {
+    Form,
+    Input,
+    Button,
+    Radio,
+    Select,
+    Cascader,
+    DatePicker,
+    InputNumber,
+    TreeSelect,
+    Switch,
+  } from 'antd';
 import {Modal} from 'antd'
+
 class open extends Component{
     constructor(){
         super();
@@ -15,7 +28,8 @@ class open extends Component{
     };
 
     render(){
-        
+     
+
         const handleOk = () => {
             this.setState({
                 isModalVisible: false,
@@ -28,9 +42,76 @@ class open extends Component{
         };
         return(
             <Modal title="Basic Modal" visible={this.state.isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
+                <Form
+                labelCol={{
+                    span: 4,
+                }}
+                wrapperCol={{
+                    span: 14,
+                }}
+                layout="horizontal"
+
+                >
+                    <Form.Item label="Form Size" name="size">
+                        <Radio.Group>
+                        <Radio.Button value="small">Small</Radio.Button>
+                        <Radio.Button value="default">Default</Radio.Button>
+                        <Radio.Button value="large">Large</Radio.Button>
+                        </Radio.Group>
+                    </Form.Item>
+                    <Form.Item label="Input">
+                        <Input />
+                    </Form.Item>
+                    <Form.Item label="Select">
+                        <Select>
+                        <Select.Option value="demo">Demo</Select.Option>
+                        </Select>
+                    </Form.Item>
+                    <Form.Item label="TreeSelect">
+                        <TreeSelect
+                        treeData={[
+                            {
+                            title: 'Light',
+                            value: 'light',
+                            children: [
+                                {
+                                title: 'Bamboo',
+                                value: 'bamboo',
+                                },
+                            ],
+                            },
+                        ]}
+                        />
+                    </Form.Item>
+                    <Form.Item label="Cascader">
+                        <Cascader
+                        options={[
+                            {
+                            value: 'zhejiang',
+                            label: 'Zhejiang',
+                            children: [
+                                {
+                                value: 'hangzhou',
+                                label: 'Hangzhou',
+                                },
+                            ],
+                            },
+                        ]}
+                        />
+                    </Form.Item>
+                    <Form.Item label="DatePicker">
+                        <DatePicker />
+                    </Form.Item>
+                    <Form.Item label="InputNumber">
+                        <InputNumber />
+                    </Form.Item>
+                    <Form.Item label="Switch" valuePropName="checked">
+                        <Switch />
+                    </Form.Item>
+                    <Form.Item label="Button">
+                        <Button>Button</Button>
+                    </Form.Item>
+                </Form>
             </Modal>
         )
     }
